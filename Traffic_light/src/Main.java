5df8360db899a188414cdd5e8226a8358a20d452
Yuri <alekseenkoyuri1989@gmail.com>
2017-03-22 06:34:07 +0300

    add -> for test git

--- Traffic_light/src/Main.java
+++ Traffic_light/src/Main.java
@@ -6,5 +6,6 @@ public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         go(); //запуск модуля
+        System.out.println("->");
     }
 }
